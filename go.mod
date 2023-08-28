a7831bffdbc284bb3748d8b6c86e72edfb0b1a76
SKi <megamanics@users.noreply.github.com>
2023-08-27 20:35:17 -0700

    - Added a new dependency `github.com/sashabaranov/go-openai v1.14.2` - Implemented functions to get the git diff and commit message prompt - Added a function to get the completion response from the OpenAI chat model - Updated the main function to call the new functions and print the completion response

--- go.mod
+++ go.mod
@@ -15,6 +15,7 @@ require (
 	github.com/mattn/go-runewidth v0.0.14 // indirect
 	github.com/muesli/termenv v0.13.0 // indirect
 	github.com/rivo/uniseg v0.4.4 // indirect
+	github.com/sashabaranov/go-openai v1.14.2 // indirect
 	github.com/thlib/go-timezone-local v0.0.0-20210907160436-ef149e42d28e // indirect
 	golang.org/x/net v0.7.0 // indirect
 	golang.org/x/sys v0.8.0 // indirect
--- main.go
+++ main.go
@@ -1,10 +1,58 @@
 package main
 
 import (
+	"context"
 	"fmt"
-
 	"github.com/cli/go-gh/v2/pkg/api"
+	openai "github.com/sashabaranov/go-openai"
+	"os"
+	"os/exec"
 )
+
+func getGitDiff() (string, error) {
+	cmd := exec.Command("git", "diff")
+	output, err := cmd.Output()
+	if err != nil {
+		return "", fmt.Errorf("error running git diff: %v", err)
+	}
+	return string(output), nil
+}
+
+func getCommitMessagePrompt() (string, error) {
+	diff, err := getGitDiff()
+	if err != nil {
+		return "", fmt.Errorf("error getting git diff: %v", err)
+	}
+
+	prompt := fmt.Sprintf("Please review the following changes:\n\n%s\n\nEnter commit message:", diff)
+	return prompt, nil
+}
+
+func getChatCompletionResponse(prompt string) (string, error) {
+	token := os.Getenv("OPENAI_API_KEY")
+	if token == "" {
+		return "", fmt.Errorf("OPENAI_API_KEY environment variable not set")
+	}
+	client := openai.NewClient(token)
+	resp, err := client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: openai.GPT3Dot5Turbo,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: prompt,
+				},
+			},
+		},
+	)
+
+	if err != nil {
+		return "", fmt.Errorf("Completion error: %v", err)
+	}
+
+	return resp.Choices[0].Message.Content, nil
+}
 
 func main() {
 	fmt.Println("hi world, this is the gh-commit extension!")
@@ -13,11 +61,22 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	response := struct {Login string}{}
+	response := struct{ Login string }{}
 	err = client.Get("user", &response)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	fmt.Printf("running as %s\n", response.Login)
+	prompt, err := getCommitMessagePrompt()
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return
+	}
+	completionResponse, err := getChatCompletionResponse(prompt)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return
+	}
+	fmt.Println(completionResponse)
 }
