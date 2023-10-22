23464427cc8a996f2df32cc12e2494696f9b47c5
SKi <megamanics@users.noreply.github.com>
2023-10-22 15:52:56 -0700

    ``` feat: Update installation script and implement Alias
    This commit includes significant updates to the install.sh script. A critical section where the user was asked to enter their OpenAI API key was removed. This section includes reading and storing the API key to a bash variable and then exporting it to the bashrc file 🗂️.
    
    In addition, the GitHub CLI extension installation line was corrected from `ghcli/commit` to `ghcli/gh-commit`. This ensures that the correct extension is installed 📝.
    
    A new Git alias called 'auto-commit' has been created. This was then appended to the bashrc file to make it globally available. Sourcing the bashrc file ensures our new alias is available instantly after installation 🌍.
    
    Updated the final output to include usage of the new auto-commit feature for the user's convenience. Let's enjoy a streamlined Git commit process now 🚀!
    ```

--- install.sh
+++ install.sh
@@ -6,23 +6,19 @@ then
     exit
 fi
 
-echo "Please enter your OpenAI API key:"
-read OPENAI_API_KEY
-echo "You entered: $OPENAI_API_KEY"
-
-echo "export OPENAI_API_KEY=$OPENAI_API_KEY" >> ~/.bashrc
-source ~/.bashrc
-
 # Print the latest release binary 
 LATEST_RELEASE=$(gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /repos/ghcli/gh-commit/releases --jq '.[0].tag_name')
 echo "Latest release: $LATEST_RELEASE"
 
 # Install GitHub CLI extension 
-gh extension install ghcli/commit 
+gh extension install ghcli/gh-commit 
 
 # Create Git alias
 git config --global alias.auto-commit '!git commit -m "$(gh commit)" || git commit -a -m "$(gh commit)" && git log HEAD...HEAD~1'
+echo "alias auto-commit='git commit -m \"\$(gh commit)\" || git commit -a -m \"\$(gh commit)\" && git log HEAD...HEAD~1'" >> ~/.bashrc
+source ~/.bashrc
 
 echo "Commit GitHub CLI extension installed successfully!"
 echo "Usage: git auto-commit"
 echo "Usage: gh commit"
+echo "Usage: auto-commit"
